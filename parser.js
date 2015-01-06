2ac5913dc7aa64d7bfc34202baca3cbbb3a9e3b1
Ziyi <ziyifeng1109@gmail.com>
2015-01-06 16:29:55 +1300

    add comments in parser.js

--- parser.js
+++ parser.js
@@ -1,3 +1,5 @@
+// data can be a single string
+//   or an array of strings
 var Parser = function(data){
 	this.tag = '';
 	if(data instanceof Array){
@@ -10,11 +12,13 @@ var Parser = function(data){
 	}
 };
 
+// return a new Parser
 Parser.feed = function(data){
 	return new Parser(data);
 };
 
 Parser.prototype = {
+	// convert the css selector to regex
 	_convert: function(pstr){
 		var rstrs = pstr.split('[');
 
@@ -28,6 +32,7 @@ Parser.prototype = {
 		}
 
 		var attrs = rstrs[1].substring(0, rstrs[1].length-1);
+		// convert attributes to regex
 		var kvstr = this._convert_kv(attrs);
 
 		var rstr = 
@@ -36,11 +41,13 @@ Parser.prototype = {
 			+ kvstr					//required attributes
 			+ '[^>]*'				//0 or more attributes
 			+ '>'					//end of start tag
-			+ '(.|\\s)*?'					//tag body
+			+ '(.|\\s)*?'			//tag body
 			+ '<\/' + tag + '>';	//end tag
 		return rstr;
 
 	},
+
+	// convert the attributes to regex
 	_convert_kv: function(kvstr){
 		var kvs = kvstr.split(' ');
 		var res = [];
@@ -55,6 +62,9 @@ Parser.prototype = {
 		}
 		return res.join('[^<>]*');
 	},
+				
+	// reconvert the regex
+	//   by replace tag body to the right number of start tags and end tags
 	_re_convert: function(rstr, num){
 		var st = '<' + this.tag + '\\s+[^>]*>';
 		var sb = '(.|\\s)*?';
@@ -71,23 +81,39 @@ Parser.prototype = {
 
 		return rstr;
 	},
+
+	// find substring based on css selectors
 	find: function(pstr, onerror){
+
+		// a single space is used to seperate selectors
+		// they are in parent-child relationship
 		var tags = pstr.split(' ');
+
 		var _parser;
 		var res = this.data;
 		for(var i = 0; i < tags.length; i++){
 			var tmp = [];
+
 			for(var j = 0; j < res.length; j++){
+				// find the child subtrings for each parent substring
 				tmp = tmp.concat(this._find(res[j], tags[i]));
 			}
 			res = tmp;
 		}
+
+		// create a new Parser from the result and return it
+		// this supports method chaining
 		_parser = new Parser(res);
 		_parser.tag = this.tag;
 		return _parser;
 	},
+
+	// handle single case for find()
 	_find: function(data, pstr, onerror){
+
+		// convert the selector to regex
 		var rstr = this._convert(pstr);
+
 		var reg = new RegExp(rstr, 'gi');
 		var matches = data.match(reg);
 		if(!matches){
@@ -101,6 +127,8 @@ Parser.prototype = {
 		var em = mat.match(new RegExp(etag, 'gi')).length;
 
 		// handle nested tag
+		// if the start tags is more than the tags, nested tags are found
+		//   so reconvert the selector
 		while(sm > em){
 			var nstr = this._re_convert(rstr, sm-1);
 			reg = new RegExp(nstr, 'gi');
@@ -111,6 +139,8 @@ Parser.prototype = {
 		}
 		return matches;
 	},
+
+	// separate the outermost tag and its innerHTML
 	_get_head_body: function(data){
 		var s = data.indexOf('>');
 		var e = data.lastIndexOf('<');
@@ -118,6 +148,8 @@ Parser.prototype = {
 		var body = data.substring(s+1, e);
 		return [head, body];
 	},
+
+	// separate key and value
 	_get_key_value: function(data){
 		var kv = data.split('=');
 		var k = kv.shift();
@@ -125,6 +157,8 @@ Parser.prototype = {
 		v = v.substring(1, v.length-1);
 		return [k,v];
 	},
+
+	// convert attrs into object
 	_get_attrs: function(data){
 		var kvs = data.split(/\s+/);
 		var attrs = {};
@@ -138,20 +172,27 @@ Parser.prototype = {
 		}
 		return attrs;
 	},
+
+	// call the given function for each data items
+	// the function is passed with innerHTML, attrs as an object, and raw data string
 	parse: function(func){
 		for(var i = 0; i < this.size; i++){
 			if(this.data[0] == ''){
 				func('', {}, '');
 			}
 			else{
+				// separate outmost tag and its innerHTML
 				var hb = this._get_head_body(this.data[i]);
 				data = this.data[i];
 				innerHTML = hb[1];
+				// convert attrs into object
 				attrs = this._get_attrs(hb[0]);
 				func(innerHTML, attrs, data);
 			}
 		}
 	},
+
+	// return a new Parser with a single data item
 	index: function(i){
 		if(this.size == 1){
 			return this;
