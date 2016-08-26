ccb6a613652e240806a457397ed3636f5830d977
Ziyi <ryanfzyw@gmail.com>
2016-08-27 11:13:04 +1200

    stringformaterjs: removed testing code, added comments

--- stringformater.js
+++ stringformater.js
@@ -1,44 +1,20 @@
-var input = "from 'https://careers.mercyascot.co.nz/home'\n" +
-            "select 'div[class=job]' as rets\n" +
-            "where-each as ret\n" +
-            "from ret.html\n" +
-            "select 'div[class=title] a' as href\n" +
-            "add-field 'https://careers.mercyascot.co.nz/${href.attrs.href}' for-key '%{HREF}'\n" +
-            "and\n" +
-            "from ret.html\n" +
-            "select 'div[class=title] a span' as title\n" +
-            "add-field title.html for-key '%{TITLE}'\n" +
-            "and\n" +
-            "from ret.html\n" +
-            "select 'div[class=description]' as description\n" +
-            "add-field description.html for-key '%{DESCRIPTION}'\n" +
-            "and\n" +
-            "from ret.html\n" +
-            "select 'span[class=detail-item]' as [%{LOCATION}, %{EXPERTISE}, %{WORKTYPE}, %{LEVEL}, %{POSTEDDATE}, %{CLOSEDATE}]\n" +
-            "where-each as detail\n" +
-            "add-field detail.html for-key detail.$Name;";
-
-var HREF = 'href';
-var TITLE = 'title';
-var DESCRIPTION = 'description';
-var LOCATION = 'location';
-var EXPERTISE = 'expertise';
-var WORKTYPE = 'workType';
-var POSTEDDATE = 'postedDate';
-var CLOSEDATE = 'closeDate';
-
 var stringformatterjs = (function(){
+    // str is the replacing string
+    // scope is an object that contains the values of the keys in the replacing string
+    // therefore, for some <key> in <str>, <scope>[<key>] has the <replacing-value>
     var formater = function(str, scope){
+        // identify for the replacing strings, e.g. %{somekey}
         var regKey = /%\{[^{}]+?\}/g;
         var matches = str.match(regKey);
         for (var i = 0; i < matches.length; i++){
             var match = matches[i];
             var key = match.substring(2, match.length-1);
+            // if scope has the value for the key, replace the key
+            // otherwise, keep as it is
             var replace = match;
             if (key in scope){
                 replace = scope[key];
             }
-            console.log('match:'+match+';repalce:'+replace);
             str = str.replace(match, replace);
         }
         return str;
@@ -48,5 +24,3 @@ var stringformatterjs = (function(){
         format : formater
     }
 })();
-
-console.log(stringformatterjs.format(input, this));
